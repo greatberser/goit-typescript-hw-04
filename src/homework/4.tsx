c346b2b4dda81a80c01b6890e95f261479e1fbde
Maks <maks135254@gmail.com>
2024-02-13 15:20:34 +0200

    Update 4.tsx

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -70,7 +70,7 @@ function MenuComponent({ menus }: PropsMenu) {
       {menus.map((menu) => (
         <div key={menu.id} onClick={() => onSelectedMenu({ id: menu.id })}>
           {menu.title}{" "}
-          {selectedMenu.id === menu.id ? "Selected" : "Not selected"}
+          {selectedMenu?.id === menu.id ? "Selected" : "Not selected"}
         </div>
       ))}
     </>
